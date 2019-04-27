820b4bbb0b8aac2c48770eb4dbe8701c683b3d3f
Brainstorm67 <Brainstorm67@users.noreply.github.com>
2019-04-28 01:15:56 +0200

    Many scripts

---
+++ Twilight Zone/Assets/Scripts/Character.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    float hp = 100f;
+    public GameObject distanceWeapon; 
+    public GameObject meleeWeapon;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void hit() 
+    {
+
+    }
+
+    void shoot(Vector3 direction)
+    {
+
+    }
+}
