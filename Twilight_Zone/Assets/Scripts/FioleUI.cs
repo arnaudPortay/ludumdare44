f88517cba1c0f0a93652a7554522fc0da5903faa
arnaudPortay <arnaud.portay@telecom-st-etienne.fr>
2019-04-28 21:26:33 +0200

    UI Behavior

---
+++ Twilight_Zone/Assets/Scripts/FioleUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FioleUI : MonoBehaviour
+{
+    public int minHeight = 130;
+    public int maxHeight = 440;
+
+    private RectTransform lRectTransform;
+
+    private void Awake() 
+    {
+        lRectTransform = gameObject.GetComponent<RectTransform>();        
+    }
+
+    // Start is called before the first frame update
+     public void addMaxHealth(float pPercentage)
+     {
+         if (lRectTransform.sizeDelta.y ==  maxHeight)
+         {
+             return;
+         }
+
+        float newY = lRectTransform.sizeDelta.y + (lRectTransform.sizeDelta.y * pPercentage)/100f;
+        // Do not go over max height
+        newY = Mathf.Min(newY, maxHeight);
+        lRectTransform.sizeDelta = new Vector2(lRectTransform.sizeDelta.x, newY);
+     }
+
+     private void Update() {
+         if (Input.GetMouseButtonDown(0))
+         {
+             addMaxHealth(14);
+         }
+     }
+}
---
+++ Twilight_Zone/Assets/Scripts/WaveletUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveletUI : MonoBehaviour
+{
+    public int TotalMaxHeight = 420;
+    public int CurrentMaxHeight;
+
+    public int height;
+
+    private RectTransform lRectTransform;
+
+    private void Awake() 
+    {
+        lRectTransform = gameObject.GetComponent<RectTransform>();        
+    }
+
+    public void addMaxHealth(float pPercentage)
+     {
+         if (TotalMaxHeight ==  CurrentMaxHeight)
+         {
+             return;
+         }
+
+        float newY = lRectTransform.sizeDelta.y + (lRectTransform.sizeDelta.y * pPercentage)/100f;
+        // Do not go over max height
+        newY = Mathf.Min(newY, TotalMaxHeight);
+        CurrentMaxHeight = (int)newY;
+     }
+
+    public void setHealth(float pHealth)
+    {
+
+    } 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+         {
+             addMaxHealth(14);
+         }
+    }
+}
