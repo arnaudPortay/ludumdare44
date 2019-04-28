0d9945fcd39f41c0a7599d63f99df04f55fc0b7a
Brainstorm67 <Brainstorm67@users.noreply.github.com>
2019-04-29 01:02:51 +0200

    Enemy controller et machine a état animation loup

--- Twilight_Zone/Assets/Scripts/Enemy.cs
+++ Twilight_Zone/Assets/Scripts/Enemy.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : Character
 {
+        Animator anim;
+        private NavMeshAgent navMesh;
+
+        private float timer = 0.0f;
+
+        public float MinDistAttack = 1.0f;
+
+        public float AttackCoolDown = 2.0f;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        navMesh = gameObject.GetComponent<NavMeshAgent>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate() 
+    {
+        timer += Time.fixedDeltaTime;
+
+        Vector3 destPos = GameObject.FindWithTag("Player").transform.position;
+        navMesh.SetDestination(destPos);
+        AnimatorStateInfo  lStateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        if(Vector3.Distance(destPos, transform.position) < MinDistAttack && timer > AttackCoolDown)
+        {
+            timer = 0.0f;
+            anim.SetTrigger("Jump");
+        }
     }
 }
